384f472b4608d835623b7ada1bbda947cacf089c
cmcotton <cmcotton@gmail.com>
2016-04-26 13:48:02 +0800

    arrange

--- src/main/java/parser/CEFParser.java
+++ src/main/java/parser/CEFParser.java
@@ -23,7 +23,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.gson.Gson;
-import com.mysql.jdbc.StringUtils;
+import com.mysql.cj.core.util.StringUtils;
 
 import db.DBConnection;
 import db.DBConnectionNative;
